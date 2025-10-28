56bebe974e68e25e8c759aa15c8ecbdb62f11c95
onedec <carlosantoniocedeno@gmail.com>
2025-10-28 14:48:59 -0400

    Completed basic functionality of SkyTree (listening post).

--- SkyTree/database/models.py
+++ SkyTree/database/models.py
@@ -3,3 +3,9 @@ from database.db import db
 #define Task object in Database
 class Task(db.DynamicDocument):
     task_id = db.StringField(required=True)
+
+class Result(db.DynamicDocument):
+    result_id = db.StringField(required=True)
+
+class TaskHistory(db.DynamicDocument):
+    task_object = db.StringField()
