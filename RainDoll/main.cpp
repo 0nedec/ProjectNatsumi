e3b48d6bf397ebec36f7517e7b9f7110ec2fb137
C Ced <carlosantoniocedeno@gmail.com>
2025-11-03 10:48:33 -0500

    Finished SkyTree. Scaffolding for RainDoll complete

---
+++ RainDoll/main.cpp
@@ -0,0 +1,30 @@
+﻿// main.cpp : Defines the entry point for the application.
+//
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include "implant.h"
+
+#include <stdio.h>
+
+#include <boost/system/system_error.hpp>
+
+int main() {
+	const auto host = "localhost";
+	const auto port = "5000";
+	const auto uri = "/results";
+
+
+	Implant implant{ host, port, uri };
+
+	try {
+		implant.beacon();
+		printf("Implant beacon successful!");
+	}
+
+	catch (const boost::system::system_error& se) {
+		printf("/nSystem error: %s/n", se.what());
+	}
+
+}
